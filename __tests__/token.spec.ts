803df77191a77fbf22f5d7a45a8b4c8eca506258
Michael Matzka <14311597+mima0815@users.noreply.github.com>
2026-06-26 15:52:07 +0200

    chore(tests): replace jest with vitest

--- __tests__/token.spec.ts
+++ __tests__/token.spec.ts
@@ -10,14 +10,14 @@ describe('test Token Class', () => {
   });
 
   test('checks the token whether it is expired or not', () => {
-    jest.useFakeTimers();
+    vi.useFakeTimers();
     const expiresAt = Math.floor(new Date('2020-01-02').getTime() / 1000);
     const token = new Token('testtoken', expiresAt);
 
-    jest.setSystemTime(new Date('2020-01-01'));
+    vi.setSystemTime(new Date('2020-01-01'));
     expect(token.isExpired()).toBe(false);
 
-    jest.setSystemTime(new Date('2020-01-03'));
+    vi.setSystemTime(new Date('2020-01-03'));
     expect(token.isExpired()).toBe(true);
   });
 });
