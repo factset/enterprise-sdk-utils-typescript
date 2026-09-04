598d92d89c3342be9df6228a91b7ce850cbee96e
Michael Matzka <14311597+mima0815@users.noreply.github.com>
2026-09-04 08:41:19 +0200

    chore(publish): setup trusted publishing (#783)

--- __tests__/openIDClientFactory.spec.ts
+++ __tests__/openIDClientFactory.spec.ts
@@ -48,7 +48,7 @@ describe('Test OpenIDClientFactory class', () => {
 
     test('should not throw an error and set proxy properly', async () => {
       const proxyUrl = 'http://proxy.example.com:8080';
-      const userAgent = `fds-sdk/javascript/utils/2.1.4 (${process.platform}; node ${process.version})`;
+      const userAgent = `fds-sdk/javascript/utils/2.1.5 (${process.platform}; node ${process.version})`;
 
       vi.mocked(Issuer.discover).mockResolvedValue({
         metadata: {
